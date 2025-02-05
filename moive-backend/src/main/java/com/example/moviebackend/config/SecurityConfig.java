b53eef74b650ae6df87f04bbde8455e1badf25c3
leeanchul <lacjesusg@gmail.com>
2025-02-05 09:14:11 +0900

    code udpate

--- moive-backend/src/main/java/com/example/moviebackend/config/SecurityConfig.java
+++ moive-backend/src/main/java/com/example/moviebackend/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
@@ -51,7 +52,7 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(HttpBasicConfigurer::disable)
-                .csrf(CsrfConfigurer::disable)
+                .csrf(AbstractHttpConfigurer ::disable)
                 .sessionManagement(session->
                         session
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
